8f6b5e78343064e28bdd5c5d62629633a9d2b4eb
Nazmul Hasan Robin <robin.sust08@gmail.com>
2024-04-19 20:52:08 +0600

    Vendor added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a600fe6366f4473c3294207eae39b5aad68fa591',
+        'name' => 'nhrrob/nhrrob-options-table-manager',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nhrrob/nhrrob-options-table-manager' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a600fe6366f4473c3294207eae39b5aad68fa591',
+            'dev_requirement' => false,
+        ),
+    ),
+);
