5217519fa1064ca48c73fa1065ada594e4caab6f
Nazmul Hasan Robin <robin.sust08@gmail.com>
2024-07-03 09:22:55 +0600

    Console removed

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,8 +1,5 @@
 (function ($){
-    console.log('alright!!');
-    
     $( document ).ready(function() {
         let table = new DataTable('.nhrotm-options-table-manager .form-table');
-        console.log(table);
     });
  })(jQuery);
