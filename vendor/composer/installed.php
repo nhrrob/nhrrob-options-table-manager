056e9b1f39c7f49c56952cf2d08943c158734657
Nazmul Hasan Robin <robin.sust08@gmail.com>
2025-03-15 20:49:41 +0600

    composer script added

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => 'dev-trunk',
-        'version' => 'dev-trunk',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'd2fb624425f2ff9b9547ceac02969a74b7af62a7',
+        'reference' => '9bd8ac4a2eaab95d56a557d0b2b218ec4711faf5',
         'name' => 'nhrotm/options-table-manager',
         'dev' => false,
     ),
     'versions' => array(
         'nhrotm/options-table-manager' => array(
-            'pretty_version' => 'dev-trunk',
-            'version' => 'dev-trunk',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'd2fb624425f2ff9b9547ceac02969a74b7af62a7',
+            'reference' => '9bd8ac4a2eaab95d56a557d0b2b218ec4711faf5',
             'dev_requirement' => false,
         ),
     ),
