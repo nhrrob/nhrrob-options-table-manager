bae4a2f6f75dec136b0df653ea95f1b0bab5d89f
Nazmul Hasan Robin <robin.sust08@gmail.com>
2026-09-25 17:41:03 +0600

    Refactor ToolsScreen table layout and improve responsiveness
    - Wrapped the backup table in a scrollable div to enhance usability on smaller screens.
    - Adjusted the table structure for better alignment and readability.
    - Updated SCSS styles to ensure proper color handling in dark mode and improved layout for mobile devices.
    - Modified Assets class to conditionally register scripts and styles only on the relevant admin page, optimizing performance.

--- admin/build/index.asset.php
+++ admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '49fc3fe8d7fa05e74a76');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '9b095c15c92d69f61407');
